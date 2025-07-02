f8bc4f96af31fdb7d5efb91d2c7a0455b38cc40c
Neacsu Andrei Rares <neacsurares2006@gmail.com>
2025-07-02 14:46:29 +0300

    test

--- day_01/day1.py
+++ day_01/day1.py
@@ -1,6 +1,6 @@
 print("Calculeaza aria unui dreptunghi cu laturile a si b")
 a = float(input("Introduceti latura a: "))
-b = float(input("Introduceti latura b: "))
+b = float(input("Introduceti latura c: "))
 def aria(a, b):
     return a * b
 arie= aria(a, b)

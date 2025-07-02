6d191b8388ee8ff8bb4162534d765c685c2c328f
Neacsu Andrei Rares <neacsurares2006@gmail.com>
2025-07-02 14:31:39 +0300

    created a folder for day 1

---
+++ day_01/test.py
@@ -0,0 +1,5 @@
+print("Hello, World!")
+nume=input("What is your name? ")
+def intampinare(nume):
+    print("Buna, " + nume + "!")
+intampinare(nume)
---
+++ day_01/test2.py
@@ -0,0 +1,5 @@
+name=input("What is your name? ")
+age=int(input("What is your age? "))
+print(type(name))
+print(type(age))
+print("Hello, " + name + "! You are " + str(age) + " years old.")
